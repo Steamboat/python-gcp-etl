a6c6cf629a40046baeba2c0df65571176efaa3d8
EverydayFuture <52001397+EverydayFuture@users.noreply.github.com>
2021-02-27 13:40:44 -0500

    Sync library with GCP tests (#1)
    * add setup.py installer
    
    * remove build files
    
    * improve gitignore
    
    * fold in changes from cloud functions and notebooks
    
    * improve setup
    
    * improve find packages
    
    * add __init__.py
    
    * bump version
    
    * remove package specification
    
    * improve import
    
    * bump version
    
    * make config dynamic
    
    * fix gbq data lake push
    
    * diagnose bq_table issues
    
    * fix bq table upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python
+
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='etl',
+    packages=setuptools.find_packages(),
+    version='0.1.8',
+    description='Templates for building Cloud Function ETL processes',
+    long_description=long_description,
+    author='Steven Sutton',
+    license='BSD',
+    author_email='admin@theeverydayfuture.com',
+    url='https://github.com/Steamboat/python-gcp-etl',
+    keywords=['postgres', 'etl', 'gcp', ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
